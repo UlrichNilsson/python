a40d3611016c396f7603f3369b46782aa92a8558
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2018-12-13 21:36:50 +0100

    Updates

--- DataScience/ImportCSV.py
+++ DataScience/ImportCSV.py
@@ -1,3 +1,10 @@
 import pandas as pd
-df = pd.read_csv("./mpg.csv")
-df
+import os
+print (os.path)
+
+#from os.path import dirname, join
+#current_dir = dirname(__file__)
+#file_path = join(current_dir, "./mpg.csv")
+
+#df = pd.read_csv(file_path)
+#df
