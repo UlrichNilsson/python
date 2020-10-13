ab3e33e6fc6b147f1d21c1f22eb9d064c29975b4
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2020-10-13 20:52:46 +0200

    Python assignments

--- PythonForEveryone/50-forloop.py
+++ PythonForEveryone/50-forloop.py
@@ -1,1 +1,9 @@
 numbers=[1,5,12,43,4,23]
+
+highestvalue=0
+
+for number in numbers:
+    if(number>highestvalue):
+        highestvalue=number
+
+print("Higest number is", highestvalue)
---
+++ PythonForEveryone/51-whileloop.py
@@ -0,0 +1,24 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done" : break
+    try:
+        fnum=int(num)
+    except:
+        print('Invalid input')
+        continue
+    if(largest is None):
+        largest=fnum
+    else:
+        if(fnum>largest):
+            largest=fnum
+            
+    if(smallest is None):
+        smallest=fnum
+    else:
+        if(fnum<smallest):
+            smallest=fnum   
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
