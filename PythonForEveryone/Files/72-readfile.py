bd74861ec4080e3713367a5f8f0e366a0c173df9
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2020-10-14 22:43:07 +0200

    assignment72

--- PythonForEveryone/Files/72-readfile.py
+++ PythonForEveryone/Files/72-readfile.py
@@ -1,7 +1,23 @@
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, 
+# looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average 
+# of those values and produce an output as shown below. Do not use the sum() function or a variable named 
+# sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing 
+# below enter mbox-short.txt as the file name.
+
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
+#fname = 'mbox-short.txt'
 fh = open(fname)
+
+result=0.0
+counter=0
+
 for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:") : continue
-    print(line)
-print("Done")
+    if line.startswith("X-DSPAM-Confidence:") : 
+        counter=counter+1
+        result=result+float(line[line.find('0'):len(line):None])
+        
+print('Average spam confidence:',result/counter)
