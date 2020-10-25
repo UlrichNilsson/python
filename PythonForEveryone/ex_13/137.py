2016a9f658100625558b319bfb1985617628f01b
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2020-10-25 20:51:41 +0100

    json examples

---
+++ PythonForEveryone/ex_13/137.py
@@ -0,0 +1,32 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+serviceurl='http://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+    address=input('Enter location : ')
+    if(len(address)<1):break
+    
+    url = serviceurl + urllib.parse.urlencode({'address':address})
+    
+    print('Retrieving',url)
+    
+    uh = urllib.request.urlopen(url)
+    
+    data = uh.read().decode()
+    
+    print('Retrieved', len(data),'characters')
+    
+    try:
+        js = data.loads(data)
+    except:
+        js = None
+    
+    if not js or 'status' not in js or js['status'] !='OK':
+        print('========Failure to retrieve=====')
+        print(data)
+        continue
+    
+    print(json.dumps(js,inent=4))
+    
+    lat = js["result"][0]["gemometry"]["location"]["lat"]
